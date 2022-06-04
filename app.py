140a32131e180621073b7804995a115ff1fec639
Diogo Carapito <diogo.carapito@icloud.com>
2022-06-04 23:02:26 +0100

    ajustes estilo

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ app.layout = html.Div([
     html.Div([
         html.Div([
             html.H3('Gastos medicmaentos por ARS entre 2017 e 2021 texto grande so para testar coisas ')
-        ], className='row', style={'display':'block'}),
+        ], className='row'),
         html.Div([
             html.Div([
                 html.H3('Filtros:'),
