7aa755c3c7c84c0c6ef7a9cbc7af8a02ab911185
Diogo Carapito <diogo.carapito@icloud.com>
2022-06-08 19:16:31 +0100

    teste com dummy treemap

--- treemap_test.py
+++ treemap_test.py
@@ -5,14 +5,7 @@ import plotly.graph_objects as go
 path = 'databases/'
 df_treemap = pd.DataFrame(data=pd.read_csv(path + 'dispensa-de-medicamentos-por-grupo-farmacoterapeutico-por-ano.csv'))
 
-
-
-values = [0, 11, 12, 13, 14, 15, 20, 30]
-labels = ["Nacional", "LVT", "A2", "Norte", "B2", 'Centro']
-parents = ["", "Nacional", "LVT", "Nacional", "Norte"]
-
 treemap_anos = list(dict.fromkeys(df_treemap['ano'].tolist()))
-
 year_slider = dcc.Slider(
     id = 'slider_ano',
     min=min(treemap_anos),
@@ -46,17 +39,26 @@ def generate_chart(slider_ano):
     lista_ars = list(dict.fromkeys(lista_ars))
 
     labels = ['Nacional']
-    values = [sum(treemap_selection['soma_encargos_sns_ambulatorio'])]
+    #values = [sum(treemap_selection['soma_encargos_sns_ambulatorio'])]
+    values = [0]
     parents = ['']
     for each in lista_ars:
         labels.append(each)
-        dados_por_ars = treemap_selection.loc[df_treemap['ars'] == each]
-        values.append(sum(dados_por_ars))
+        dados_por_ars = treemap_selection.loc[treemap_selection['ars'] == each]
+        #values.append(sum(dados_por_ars['soma_encargos_sns_ambulatorio']))
+        values.append(0)
+
         parents.append('Nacional')
         for cada in lista_grupos_terapeuticos:
             labels.append(cada)
-            values.append(dados_por_ars[cada])
+            
+            gasto_ars_grupo = dados_por_ars.loc[dados_por_ars['grupo_terapeutico']==cada]['soma_encargos_sns_ambulatorio']
+            values.append(gasto_ars_grupo.to_string(index=False))
+            #values.append(float(gasto_ars_grupo.to_string(index=False)))
+            #values.append(4000000.0)
+            
             parents.append(each)
+
 
     fig = go.Figure(go.Treemap(
         labels = labels,
