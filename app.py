8b3556c3bb2d1be1d1d4978875fcc2cb7602dbc5
Diogo Carapito <diogo.carapito@icloud.com>
2022-06-09 22:32:46 +0100

    avanços significativos

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objs as go
 
 path = 'databases/'
-df_gasto_medicamentos_por_ano = pd.DataFrame(data=pd.read_csv(path + 'despesa-com-medicamentos-no-sns-por-ano.csv'))
+df_despesa_com_medicamentos_no_sns_por_ano_por_regiao = pd.DataFrame(data=pd.read_csv(path + 'despesa_com_medicamentos_no_sns_por_ano_por_regiao.csv'))
 
 path = 'databases/'
-df_gasto_medicamentos = pd.DataFrame(data=pd.read_csv(path + 'despesa-com-medicamentos-no-sns-por-ano-por-regiao.csv'))
+df_despesa_com_medicamentos_no_sns_por_ano = pd.DataFrame(data=pd.read_csv(path + 'despesa-com-medicamentos-no-sns-por-ano.csv'))
+
+
 
 path = 'databases/'
 df_treemap = pd.DataFrame(data=pd.read_csv(path + 'dispensa-de-medicamentos-por-grupo-farmacoterapeutico-por-ano-por-regiao.csv'))
-
-
 path = ''
 df_bar_chart = pd.DataFrame(data=pd.read_csv(path + 'bar_chart_dummy.csv'))
+
+
 
 dropdown_ars_gasto_medicamentos_1 = [
     {'label': 'Nacional', 'value': 'nacional'},
@@ -24,13 +26,17 @@ dropdown_ars_gasto_medicamentos_1 = [
     {'label': 'Algarve', 'value': 'algarve'},
 ]
 
-## aqui o value tem que ser em integer porque aparentemente a exctração do CSV inicial interpreta os valores numéricos como integer, e para a logica lá em baixo funcionar tem que cuspir um inteiro e não um texto, ou então teria de necessitar de uma reconversão str par int
+#Total = Hospitalar + Ambulatório PVP
+#Ambulatório PVP = Ambulatório Encargo Utentes + Ambulatório Comparticipalção SNS
+
 dropdown_ars_gasto_medicamentos_2 = [
-    {'label': '2017', 'value': 2017},
-    {'label': '2018', 'value': 2018},
-    {'label': '2019', 'value': 2019},
-    {'label': '2020', 'value': 2020},
-    {'label': '2021', 'value': 2021},
+    {'label': 'Total', 'value': 'total'},
+    {'label': 'Hospitalar', 'value': 'encargos_sns_hospitalar'},
+    {'label': 'Ambulatório PVP', 'value': 'valor_pvp_ambulatorio'},
+    {'label': 'Ambulatório Comparticipalção SNS', 'value': 'encargos_sns_ambulatorio'},
+    {'label': 'Ambulatório Encargo Utentes', 'value': 'encargos_utentes_ambulatorio'},
+    {'label': 'Ambulatório Comparticipalção SNS - Genéricos', 'value': 'encargos_sns_genericos'},
+
 ]
 
 app = Dash(__name__)
@@ -84,7 +90,7 @@ app.layout = html.Div([
                 dcc.Dropdown(
                     id='dropdown_ars_gasto_medicamentos_2',
                     options=dropdown_ars_gasto_medicamentos_2,
-                    value=2021
+                    value='total'
                 )
 
             ], className='col2 ', style={'text-align': 'left','width':'19%','float': 'left'}),
@@ -135,21 +141,52 @@ app.layout = html.Div([
 ])
 
 @app.callback(
+    Output("line_chart_1", "figure"),
     Output("barchart_gasto_medicamentos_1", "figure"),
     Output("barchart_gasto_medicamentos_2", "figure"),
-    Output("line_chart_1", "figure"),
     Input("dropdown_ars_gasto_medicamentos_1", "value"),
     Input("dropdown_ars_gasto_medicamentos_2", "value")
 )
 
 def generate_chart(dropdown_ars_barchart_1,dropdown_ars_barchart_2,):
+    
+    # line_chart_1
+
+    gasto_medicamentos_nacional_por_ano = df_despesa_com_medicamentos_no_sns_por_ano.sort_values(by='ano', ascending=True)
+    gasto_medicamentos_regiao_por_ano = df_despesa_com_medicamentos_no_sns_por_ano_por_regiao.sort_values(by='ano', ascending=True)
+    lista_de_regioes = gasto_medicamentos_regiao_por_ano['regiao'].tolist()
+    lista_de_regioes = list(dict.fromkeys(lista_de_regioes))
+
+    line_chart_1 = go.Figure()
+    line_chart_1.add_trace(go.Scatter(x=gasto_medicamentos_regiao_por_ano['ano'],
+                                      y=gasto_medicamentos_regiao_por_ano['encargos_sns_ambulatorio'],
+                                      mode='lines+markers',
+                                      name=regiao,
+                                      line_shape='spline'))
+
+    for regiao in lista_de_regioes:
+        ## codigo para filtrar (*.loc[*] == *)por ARS ao longo da lista de ars e alicação a uma linha no linechart
+        gastos_por_regiao = gasto_medicamentos_ambulatorio.loc[gasto_medicamentos_ambulatorio['regiao'] == regiao]
+
+        ## parte que adiciona cada uma das linhas
+        line_chart_1.add_trace(go.Scatter(x=gastos_por_regiao['ano'],
+                                          y=gastos_por_regiao['encargos_sns_ambulatorio'],
+                                          mode='lines+markers',
+                                          name=regiao,
+                                          line_shape='spline'))
+
+    line_chart_1.update_layout(title='Evolução dos gastos entre 2017 e 2021',
+                               xaxis_title='Anos',
+                               yaxis_title='Gastos em milhões de €',
+                               paper_bgcolor='#FFFFFF')
+
 
     # barchart_1
     ## filtro aplicado com a função .loc[] com logica para ter apenas dados da ars selecionada no dropdown menu correspondente
     if dropdown_ars_barchart_1 == 'nacional':
-        gasto_medicamentos = df_gasto_medicamentos.loc[df_gasto_medicamentos['regiao'] == dropdown_ars_barchart_1]
+        gasto_medicamentos = df_despesa_com_medicamentos_no_sns_por_ano_por_regiao.loc[df_despesa_com_medicamentos_no_sns_por_ano_por_regiao['regiao'] == dropdown_ars_barchart_1]
     else:
-        gasto_medicamentos = df_gasto_medicamentos_por_ano
+        gasto_medicamentos = df_despesa_com_medicamentos_no_sns_por_ano
 
 
     layout_bar_1 = dict(title=dict(text='Gastros entre 2017 e 2021'),
@@ -166,9 +203,9 @@ def generate_chart(dropdown_ars_barchart_1,dropdown_ars_barchart_2,):
     # barchart_2
     ## filtro aplicado com a função .loc[] com logica para ter apenas dados do ano selecionado no dropdown menu correspondente
     if dropdown_ars_barchart_1 == 'nacional':
-        gasto_medicamentos = df_gasto_medicamentos.loc[df_gasto_medicamentos['regiao'] == dropdown_ars_barchart_1]
+        gasto_medicamentos = df_despesa_com_medicamentos_no_sns_por_ano_por_regiao.loc[df_despesa_com_medicamentos_no_sns_por_ano_por_regiao['regiao'] == dropdown_ars_barchart_1]
     else:
-        gasto_medicamentos = df_gasto_medicamentos
+        gasto_medicamentos = df_despesa_com_medicamentos_no_sns_por_ano_por_regiao
 
     layout_bar_2 = dict(title=dict(text='evolução em percentagem'),
                         yaxis=dict(title='Gastos em milhões de €'),
@@ -182,38 +219,7 @@ def generate_chart(dropdown_ars_barchart_1,dropdown_ars_barchart_2,):
     bar_chart_2.update_layout(barmode="relative")
     #layout_yaxis_range=[0, max(gasto_medicamentos['encargos_sns_ambulatorio'])]
 
-    # line_chart_1
-    ## https://plotly.com/python/line-charts/
-
-    ## ordenar o dataframe por ordem descendente de gastos para a legenda ficar bem
-    ## https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html
-    gasto_medicamentos_ambulatorio = df_gasto_medicamentos.sort_values(by='ano', ascending=False)
-
-    ## a partir do dataframe original, saber qual é a lista de ARSs (.tolist) e remover os duplicados (.fromkeys)
-    ## https://www.geeksforgeeks.org/get-a-list-of-a-particular-column-values-of-a-pandas-dataframe/
-    ## https://www.w3schools.com/python/python_howto_remove_duplicates.asp
-    lista_de_regioes = gasto_medicamentos_ambulatorio['regiao'].tolist()
-    lista_de_regioes = list(dict.fromkeys(lista_de_regioes))
-    line_chart_1 = go.Figure()
-
-    ## loop para percorrer cada uma das ARSs e desenhar a linha correspondente
-    ## https://plotly.com/python/line-charts/
-    for regiao in lista_de_regioes:
-
-        ## codigo para filtrar (*.loc[*] == *)por ARS ao longo da lista de ars e alicação a uma linha no linechart
-        gastos_por_regiao = gasto_medicamentos_ambulatorio.loc[gasto_medicamentos_ambulatorio['regiao']==regiao]
-
-        ## parte que adiciona cada uma das linhas
-        line_chart_1.add_trace(go.Scatter(x=gastos_por_regiao['ano'],
-                                          y=gastos_por_regiao['encargos_sns_ambulatorio'],
-                                          mode='lines+markers',
-                                          name=regiao,
-                                          line_shape='spline'))
-
-    line_chart_1.update_layout(title='Evolução dos gastos entre 2017 e 2021',
-                      xaxis_title='Anos',
-                      yaxis_title='Gastos em milhões de €',
-                      paper_bgcolor = '#FFFFFF')
+    
 
 
     ## Execução dos diferentes gráficos
